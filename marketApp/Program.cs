8505ba86e95b3959adb94e15e7099c9bd3cef33f
Aya Taha <aya.taha@grinta.ai>
2024-05-18 14:13:52 +0300

    commit

--- marketApp/Program.cs
+++ marketApp/Program.cs
@@ -1,3 +1,17 @@
 ﻿
 
-Console.WriteLine("Hello, World!");
+using marketApp;
+
+JoinMyCharArray(new char[] {'A','y','a' });
+Person p = new()
+{
+    Name = "A",
+    Age = 10
+};
+Console.WriteLine(p.Name);
+
+static void JoinMyCharArray(char[] mine)
+{
+    Console.WriteLine( string.Join(' ', mine));
+
+}
